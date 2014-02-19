358f3d872b2f8c29c4d1657572aeff192c643771
Andreas Malling Østergaard <andreas.malling@gmail.com>
2014-02-19 12:42:03 +0100

    Add Communication.java for controller-layer

---
+++ src/Communication.java
@@ -0,0 +1,38 @@
+public class Communication {
+
+    static final String key = "CB91051165CC463A0975655C";
+
+    
+    public ArrayList<Item> getItems(){
+
+    }
+
+    /**
+     *
+     *  returns "OK" or "WRONG"
+     */
+    public String modifyItem(Item i){
+
+    }
+
+    /**
+     *
+     *  returns "OK" or "WRONG"
+     */
+    public String createItem(Item i){
+
+    }
+
+    /**
+     *
+     *  returns "OK" or "WRONG"
+     */
+    public String adjustItem(Item i){
+
+    }
+
+    private bool validate(Item i){
+        
+    }
+}
+
